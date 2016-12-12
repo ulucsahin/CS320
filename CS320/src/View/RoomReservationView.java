6f9180aeafae1ccac367689c1a16f17d7c3fa33f
Navhkrin <e.balatkan@gmail.com>
2016-12-12 22:51:03 +0300

    ImprovingBase
    Modified the view in such way that it is now extremely easy to add
    buttons, all the necessary checks are automatically being done in
    abstract class and only thing coder needs to do is defining what to do
    when button is clicked.

--- CS320/src/View/RoomReservationView.java
+++ CS320/src/View/RoomReservationView.java
@@ -2,15 +2,17 @@ package View;
 
 import java.awt.Graphics;
 
-public class RoomReservationView implements AbstractView {
+public class RoomReservationView extends AbstractView {
 
 	public void paint(Graphics g) {
 	}
 
 	public void mouseMoved(int x, int y) {
+		super.mouseMoved(x, y);
 	}
 
 	public void mousePressed(int x, int y) {
+		super.mousePressed(x, y);
 	}
 
 }
