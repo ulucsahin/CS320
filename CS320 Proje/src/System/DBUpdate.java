f0153c26d9e40ac0dc84b062203626fffe8388cc
berkaybayram <berkay.bayram@ozu.edu.tr>
2016-12-13 18:27:54 +0200

    DBUpdate
    - update method changed to addReservation.
    - removeReservation method added.
    - code indentation has been revised.

--- CS320 Proje/src/System/DBUpdate.java
+++ CS320 Proje/src/System/DBUpdate.java
@@ -13,84 +13,109 @@ import org.jdom.output.Format;
 import org.jdom.output.XMLOutputter;
 
 public class DBUpdate {
+	private final String FILE = "c:\\roomDBxml.xml";
+	private final String RESERVATION_ID = "reservationID";
+	private final String RESERVATIONS = "Reservations";
+	private final String ID = "ID";
 	private File xmlFile;
 	private SAXBuilder builder;
+	private Document doc;
 
 	public DBUpdate() {
 		xmlFile = null;
 		builder = null;
+		doc = null;
 		try {
 			builder = new SAXBuilder();
-			xmlFile = new File("roomDBxml.xml");
+			xmlFile = new File(FILE);
+			doc = (Document) builder.build(xmlFile);
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println("Unable to find the xml file.");
 		}
 	}
 
-	public boolean update(int buildingID, String roomID, Reservation reservation) {
+	public boolean addReservation(int buildingID, String roomID, Reservation reservation) {
 		boolean success = false;
-		Document doc;
-		try {
-			doc = (Document) builder.build(xmlFile);
-			Element rootNode = doc.getRootElement();
-			System.out.println(rootNode);
 
-			Element buildings = rootNode.getChild("Buildings");
-			System.out.println(buildings);
-
-			List building = buildings.getChildren();
-			System.out.println(building);
-
-			List rooms = null;
-			for (int i = 0; i < building.size(); i++) {
-				if (buildingID == Integer.parseInt(((Element) building.get(i)).getChild("ID").getText())) {
-					rooms = (List) ((Element) building.get(i)).getChild("Rooms").getChildren();
-				}
+		Element reservations = getReservations(buildingID, roomID);
+		List reservationsToList = reservations.getChildren();
+		Element reservationXML = null;
+		for (int i = 0; i < reservationsToList.size(); i++) {
+			if (reservation.getReservationID() == Integer
+					.parseInt(((Element) reservationsToList.get(i)).getChild(RESERVATION_ID).getText())) {
+				reservationXML = (Element) reservationsToList.get(i);
 			}
-			if (rooms == null) {
-				System.out.println("could not find the rooms");
-			}
-			System.out.println(rooms);
+		}
+		if (reservationXML == null) {
+			Element newReservationXML = new Element("Reservation");
+			newReservationXML.addContent(new Element(RESERVATION_ID).setText(reservation.getReservationID() + ""));
+			newReservationXML.addContent(new Element("timeInterval").setText(reservation.getTimeInterval() + ""));
+			newReservationXML.addContent(new Element("username").setText(reservation.getStudent().getUsername() + ""));
+			reservations.addContent(newReservationXML);
+			success = true;
+		}
 
-			Element room = null;
-			for (int i = 0; i < rooms.size(); i++) {
-				if (((Element) rooms.get(i)).getChild("ID").getText().equals(roomID)) {
-					room = ((Element) rooms.get(i));
-				}
-			}
-			if (room == null) {
-				System.out.println("room does not exist!");
-			}
-			System.out.println(room);
+		writeToXML();
+		return success;
+	}
 
-			List reservations = room.getChild("Reservations").getChildren();
-			System.out.println(reservations);
+	private Element getReservations(int buildingID, String roomID) {
 
-			Element reservationXML = null;
-			for (int i = 0; i < reservations.size(); i++) {
-				if (reservation.getReservationID() == Integer
-						.parseInt(((Element) reservations.get(i)).getChild("reservationID").getText())) {
-					reservationXML = (Element) reservations.get(i);
-				}
+		Element rootNode = doc.getRootElement();
+
+		Element buildings = rootNode.getChild("Buildings");
+
+		List building = buildings.getChildren();
+
+		List rooms = null;
+		for (int i = 0; i < building.size(); i++) {
+			if (buildingID == Integer.parseInt(((Element) building.get(i)).getChild(ID).getText())) {
+				rooms = (List) ((Element) building.get(i)).getChild("Rooms").getChildren();
 			}
-			if (reservationXML == null) {
-				Element newReservationXML = new Element("Reservation");
-				newReservationXML.addContent(new Element("reservationID").setText(reservation.getReservationID() + ""));
-				newReservationXML.addContent(new Element("timeInterval").setText(reservation.getTimeInterval() + ""));
-				newReservationXML
-						.addContent(new Element("username").setText(reservation.getStudent().getUsername() + ""));
-				room.getChild("Reservations").addContent(newReservationXML);
+		}
+		if (rooms == null) {
+			System.out.println("could not find the rooms");
+		}
+
+		Element room = null;
+		for (int i = 0; i < rooms.size(); i++) {
+			if (((Element) rooms.get(i)).getChild(ID).getText().equals(roomID)) {
+				room = ((Element) rooms.get(i));
+			}
+		}
+		if (room == null) {
+			System.out.println("room does not exist!");
+		}
+
+		return room.getChild(RESERVATIONS);
+	}
+
+	public boolean removeReservation(int buildingID, String roomID, Reservation reservation) {
+		boolean success = false;
+		Element reservations = getReservations(buildingID, roomID);
+		List reservationsToList = reservations.getChildren();
+
+		for (int i = 0; i < reservationsToList.size(); i++) {
+			if (reservation.getReservationID() == Integer
+					.parseInt(((Element) reservationsToList.get(i)).getChild(RESERVATION_ID).getText())) {
+				reservations.removeContent((Element) reservationsToList.get(i));
 				success = true;
+				break;
 			}
+		}
 
-			XMLOutputter xmlOutput = new XMLOutputter();
-			xmlOutput.setFormat(Format.getPrettyFormat());
-			xmlOutput.output(doc, new FileWriter("roomDBxml.xml"));
+		writeToXML();
+		return success;
+	}
 
-		} catch (JDOMException | IOException e) {
+	private void writeToXML() {
+		XMLOutputter xmlOutput = new XMLOutputter();
+		xmlOutput.setFormat(Format.getPrettyFormat());
+		try {
+			xmlOutput.output(doc, new FileWriter(FILE));
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return success;
 	}
 }
