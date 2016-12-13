dadf0d69f8d4dba4ace0a63fce907cafae4db88a
Navhkrin <e.balatkan@gmail.com>
2016-12-13 21:57:04 +0300

    Added current v of view

--- CS320 Proje/src/View/RoomReservationView.java
+++ CS320 Proje/src/View/RoomReservationView.java
@@ -1,16 +1,26 @@
 package View;
 
 import java.awt.Graphics;
+import java.util.ArrayList;
 
-public class RoomReservationView implements AbstractView {
+import System.Room;
+
+public class RoomReservationView extends AbstractView {
+	
 
 	public void paint(Graphics g) {
 	}
+	
+	public void buildRooms(ArrayList<Room> rooms){
+		//TODO fix dis shit
+	}
 
 	public void mouseMoved(int x, int y) {
+		super.mouseMoved(x, y);
 	}
 
 	public void mousePressed(int x, int y) {
+		super.mousePressed(x, y);
 	}
 
 }
