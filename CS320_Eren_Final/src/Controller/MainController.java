48a8542af0907871cd88cbdfd421c4c4041ce518
Navhkrin <e.balatkan@gmail.com>
2016-12-14 02:51:44 +0300

    UpdatedView
    UpdatedView, almost finished

---
+++ CS320_Eren_Final/src/Controller/MainController.java
@@ -0,0 +1,42 @@
+package Controller;
+
+import java.util.ArrayList;
+
+import System.Reservation;
+import System.Room;
+
+public class MainController {
+	private LoginController loginController;
+	private ReservationController reservationController;
+	private RoomController roomController;
+	
+	public MainController(){
+		loginController = new LoginController();
+		reservationController = new ReservationController();
+		roomController = new RoomController();
+	}
+	
+	public boolean login(String username, String password){
+		return loginController.login(username, password);
+	}
+	
+	public ArrayList<Room> getRooms(){
+		return roomController.getRooms();
+	}
+	
+	public void reserveRoom(String userID, int buildingID, String RoomID, int timeInterval) {
+		reservationController.reserveRoom(userID, buildingID, RoomID, timeInterval);
+	}
+	
+	public void reserveRoom(String userID, String RoomID, int timeInterval) {
+		reservationController.reserveRoom(userID, RoomID, timeInterval);
+	}
+
+	public void removeReservation(int buildingID, String RoomID, Reservation reservation) {
+		reservationController.removeReservation(buildingID, RoomID, reservation);
+	}
+	public void removeReservation(Reservation reservation){
+		reservationController.removeReservation(reservation);
+	}
+	
+}
